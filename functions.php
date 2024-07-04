1d053fcbde955506b4d2c951ef42d8ad5f519c01
encoderit-touhid <touhid@encoderit.net>
2024-07-04 13:52:31 +0600

    to harun vaia

--- functions.php
+++ functions.php
@@ -9,8 +9,15 @@ function my_theme_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+    
+    
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 
 
+function prefix_add_footer_styles() {
+    //wp_enqueue_style('child-style-2', get_template_directory_uri() . '/custom_style.css', array('parent-style'), '1.0.0', 'all');
+    wp_enqueue_style('extra-child-style', get_stylesheet_directory_uri() . '/custom_style.css', array('parent-style', 'child-style'), '1.0', 'all');
+};
+add_action( 'get_footer', 'prefix_add_footer_styles' );
